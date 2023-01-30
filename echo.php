2ed0605dab93f696b6e238abd0821ffa3d33ddbb
junkfreaknet <junk.freak.net@gmail.com>
2023-01-30 18:41:15 +0900

    add php file

---
+++ echo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta>charset="UTF-8"</meta>
+            <title>PHP</title>
+            <link rel="stylesheet" href="./css/index.css">
+        </head>
+        <body>
+            <?php
+                echo "<h1>". "PHP". "</h1>"
+            ?>
+        </body>
+    </html>
