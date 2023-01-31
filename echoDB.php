cc6c65a5d1f596beeea6a4b8dafe1fd977dc3bf9
junkfreaknet <junk.freak.net@gmail.com>
2023-01-31 14:30:53 +0900

    2023/01/31 14:30

--- echoDB.php
+++ echoDB.php
@@ -9,6 +9,7 @@
         <header>
             <h1>PHP DB</h1>
         </header>
+        
         <main>
             <!--- START put from database --->
             <?php
@@ -21,18 +22,29 @@
 
                 $my_connection= mysqli_connect(my_HOST,my_USER,my_PASSWORD,my_DATABASE);
                 if ($my_connection->connect_errno) {
-                    die("Connect to database is failed. ". $my_connection->connect_errno);
+                    die("Connect to database is failed: ". $my_connection->connect_errno);
                 }
 
-                echo("<p>". "Connection success!". "</p>");
+                echo("<p>". "Connection is created successful!". "</p>");
 
+                $my_sqlstring = "select * from in_tbl";
+                $rst_in = $my_connection->query("");
+
+                if ($rst_in->num_rows>0){
+                    while ($each_row=$rst_in->fetch_assoc()){
+                     echo($each_row["code"]. ",". $each_row["name"]. ",". $each_row["qty"]. "<br>");   
+                    }
+                }else{
+                    echo ("<p>" . "Data is not found." . "/p>");
+                }
                 /* at last close connection.*/
                 mysqli_close($my_connection);
-                echo("<p>". "Close database.". "</p>");
+                echo("<p>". "Close database...". "</p>");
             ?>
             <!--- END put from database --->
         </main>
+
         <footer>
         </footer>
-    </body>>
-</html>>
+    </body>
+</html>
